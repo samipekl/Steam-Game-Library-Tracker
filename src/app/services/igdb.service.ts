1a9d7352a689dc55ca7a09f3c4baa3053798479b
Santtu Taskinen <Santtu Taskinen>
2017-02-28 09:27:14 +0200

    Poking steamworks

--- src/app/services/igdb.service.ts
+++ src/app/services/igdb.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers, RequestOptions} from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class IGDBService {
+private url: string = `https://igdbcom-internet-game-database-v1.p.mashape.com/games/1942?fields=name`
 
-  constructor() { }
 
+  constructor(private http: Http) { }
+getGames = () => {
+  const headers = new Headers({ 'X-Mashape-Key': '38RcdzIK0zmshlymrWXuCBMp3GJXp16Wa0Njsnq2labY9sILFJ' });
+  const options = new RequestOptions({headers: headers});
+    return this.http.get(this.url, options)
+    .map(resp => resp.json());
+  }
 }
+
--- src/app/services/steamworks.service.ts
+++ src/app/services/steamworks.service.ts
@@ -4,15 +4,16 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SteamworksService {
-private userId: any = localStorage.getItem('id');
+private userId: any = '76561198030015039';
   private key: any = '9646ACD0D9D80E4ABCF5059BF3C7BA79';
-private url: string = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${this.key}&include_appinfo=1&include_played_free_games=1&steamid=${this.userId}`
+private url: string = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${this.key}&include_appinfo=1&include_played_free_games=1&steamid=${this.userId}`
 
 
   constructor(private http: Http) { }
 getGames = () => {
   const headers = new Headers({ 'Content-type': 'application/json' });
   const options = new RequestOptions({headers: headers});
+  this.resp.setHeader()
     return this.http.get(this.url, options)
     .map(resp => resp.json());
   }
