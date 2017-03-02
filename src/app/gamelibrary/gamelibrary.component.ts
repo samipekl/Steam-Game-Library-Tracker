1b89ec27dc9d1348b7bf4264bb32a3c8af4c9141
Santtu Taskinen <Santtu Taskinen>
2017-03-02 11:24:48 +0200

    Bleep Bloop damn for loops...

--- src/app/gamelibrary/gamelibrary.component.ts
+++ src/app/gamelibrary/gamelibrary.component.ts
@@ -11,38 +11,40 @@ import { Component, OnInit } from '@angular/core';
 export class GamelibraryComponent implements OnInit {
 
   private games: any = {};
-  private logged = 0;
   private i: any = 0;
   private id: any = {};
   private gamelog: any = [];
   private gName: string;
+  private log: any = {};
 
   constructor(private steamWorks: SteamworksService, private IGDB: IGDBService, private router: Router) { }
 
 
 
   ngOnInit() {
-this.logged = 0;
 if (localStorage.getItem('id') === null) {
     this.router.navigate(['gsearch']);
 } else {
-    console.log(localStorage.getItem('id'));
     this.id = JSON.parse(localStorage.getItem('id'));
     this.steamWorks.setUser(this.id.steamId);
     this.steamWorks.getGames().subscribe(
       (res) => {
-        console.log(res);
         this.games = res.response.games;
-        console.log(this.games)
-        console.log(this.games[0].name);
-        for ( this.i = 0; this.i < this.games.length; this.i = this.i + 1 ) {
+        /*console.log(this.games)*/
+        for ( this.i = 0; this.i < 3; this.i = this.i + 1 ) {
           console.log(this.i);
           console.log(this.games[this.i].name);
           if (this.games[this.i].name !== null) {
     this.gName = this.games[this.i].name;
     this.IGDB.getGames(this.games[this.i].name).subscribe(
       (res) => {
-        console.log(res[0]);
+        this.log = res;
+        console.log(this.log[0].genres);
+        this.games[this.i].keywords = this.log[0].keywords;
+        this.games[this.i].genres = this.log[0].genres[0];
+        this.games[this.i].themes = this.log[0].themes[0];
+        console.log(this.games[this.i]);
+    console.log('Choochoo');
       }
     );
           } else {
@@ -51,10 +53,11 @@ if (localStorage.getItem('id') === null) {
 
 }
 
+        /*console.log(this.games[4]);*/
+
       }
     );
 }
   }
-
 }
 
--- src/app/services/igdb.service.ts
+++ src/app/services/igdb.service.ts
@@ -9,7 +9,7 @@ private url: string = ``
 
   constructor(private http: Http) { }
 getGames = (gamename) => {
-  this.url = `https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search=${gamename}&fields=id,keywords,themes,genres`
+  this.url = `https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search=${gamename}&fields=id,name,keywords,themes,genres`
   const headers = new Headers({ 'X-Mashape-Key': '38RcdzIK0zmshlymrWXuCBMp3GJXp16Wa0Njsnq2labY9sILFJ' });
   const options = new RequestOptions({headers: headers});
     return this.http.get(this.url, options)
